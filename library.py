18c23b973589711abe56094b08fdabcd5b8e7cc2
Husain <husaincementwalas@gmail.com>
2025-02-28 23:03:48 +0530

    in processes

--- library.py
+++ library.py
@@ -79,17 +79,72 @@ class LibraryManagementSystem:
     txtPRN_NO.grid(row=8,column=1)
 
 
-   
+    lblBookId = Label(DataFrameLeft,bg="powder blue",text="Book Id:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblBookId.grid(row=0,column=2,sticky=W)
+    txtBookId = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtBookId.grid(row=0,column=3)
 
 
-   
+    lblBookTitle = Label(DataFrameLeft,bg="powder blue",text="Book Title:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblBookTitle.grid(row=1,column=2,sticky=W)
+    txtBookTitle = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtBookTitle.grid(row=1,column=3)
 
 
+    lblAuthor = Label(DataFrameLeft,bg="powder blue",text="Author Name",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblAuthor.grid(row=2,column=2,sticky=W)
+    txtAuthor = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtAuthor.grid(row=2,column=3)
 
 
+    lblDateBorrowed = Label(DataFrameLeft,bg="powder blue",text="Borrowed On:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblDateBorrowed.grid(row=3,column=2,sticky=W)
+    txtDateBorrowed = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtDateBorrowed.grid(row=3,column=3)
 
-    DataFrameRight = LabelFrame(frame,text="Book Details",bg="powder blue",fg="black", bd=12,relief=RIDGE, font=("helvetica",12,"bold"))
-    DataFrameRight.place(x=910,y=5,width=540,height=350)
+
+    lblDateDue = Label(DataFrameLeft,bg="powder blue",text="Due On",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblDateDue.grid(row=4,column=2,sticky=W)
+    txtDateDue = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtDateDue.grid(row=4,column=3)
+
+
+    lblDaysOnBook = Label(DataFrameLeft,bg="powder blue",text="Days On Book:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblDaysOnBook.grid(row=5,column=2,sticky=W)
+    txtDaysOnBook = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtDaysOnBook.grid(row=5,column=3)
+
+
+    lblLateReturnFine = Label(DataFrameLeft,bg="powder blue",text="Late Return Fine:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblLateReturnFine.grid(row=6,column=2,sticky=W)
+    txtLateReturnFine = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtLateReturnFine.grid(row=6,column=3)
+
+
+    lblDateOverDue = Label(DataFrameLeft,bg="powder blue",text="Date Overdue:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblDateOverDue.grid(row=7,column=2,sticky=W)
+    txtDateOverDue = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtDateOverDue.grid(row=7,column=3)
+
+
+    lblActualPrice = Label(DataFrameLeft,bg="powder blue",text="Actual Price:",font=("helvetica",12,"bold"),padx=2,pady=6)
+    lblActualPrice.grid(row=8,column=2,sticky=W)
+    txtActualPrice = Entry(DataFrameLeft,font=("helvetica",12,"bold"),width=29)
+    txtActualPrice.grid(row=8,column=3)
+
+#================================================DATA FRAME RIGHT========================================
+
+    DataFrameRight = LabelFrame(frame,text="Book Details",bg="powder blue",fg="black",padx=20, bd=12,relief=RIDGE, font=("helvetica",12,"bold"))
+    DataFrameRight.place(x=870,y=5,width=580,height=350)
+
+
+    self.txtBox = Text(DataFrameRight,font=("helvetica",12,"bold"),width=32,height=16,padx=2,pady=6)
+    self.txtBox.grid(row=0,column=2)
+
+    listBooks = ['Python Crash Course','Automate the Boring Stuff with Python','Learning Python','Fluent Python','Effective Python','Python Cookbook','Think Python','Python Tricks','Serious Python','Introduction to Machine Learning with Python','Clean Code','The Pragmatic Programmer','Design Patterns: Elements of Reusable Object-Oriented Software','Code Complete','You Dont Know JS','Eloquent JavaScript','Grokking Algorithms','Structure and Interpretation of Computer Programs','The Art of Computer Programming','Refactoring: Improving the Design of Existing Code']
+
+    listBox = Listbox(DataFrameRight,font=("helvetica",12,"bold"),width=16,height=16)
+    listBox.grid(row=0,column=0,padx=4)
 
     #==============================BUTTON FRAMES=============================
 
